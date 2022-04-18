76bb905b75525ed10629b5616f027a2b21bb7694
DarknessYamii <andresruizdelgado12@gmail.com>
2022-04-18 18:55:09 +0200

    first commit

---
+++ src/main/java/com/yandc/nightcityportalside/NightCityPortalSideApplication.java
@@ -0,0 +1,13 @@
+package com.yandc.nightcityportalside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightCityPortalSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightCityPortalSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yandc/nightcityportalside/NightCityPortalSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yandc.nightcityportalside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightCityPortalSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
